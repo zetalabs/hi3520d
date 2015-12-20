0c8a574cdee4afebbbc6b4ca7dd6d04a1164ddf7
Zeta Labs <zetalabs@gmail.com>
2015-12-20 20:48:26 +0800

    modify ezcd/lib/exported_symbols to add utils_file_get_content

--- osdrv/ezcfg/ezcfg-0.1/ezcd/src/rcso/env_action_ezcd.c
+++ osdrv/ezcfg/ezcfg-0.1/ezcd/src/rcso/env_action_ezcd.c
@@ -1,3 +1,4 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 /* ============================================================================
  * Project Name : ezbox Configuration Daemon
  * Module Name  : env_action_ezcd.c
@@ -42,13 +43,15 @@
 #include "ezcd.h"
 #include "pop_func.h"
 
-#if 0
-#define DBG(format, args...) do {\
-	FILE *fp = fopen("/dev/kmsg", "a"); \
-	if (fp) { \
-		fprintf(fp, format, ## args); \
-		fclose(fp); \
-	} \
+#if 1
+#define DBG(format, args...) \
+do { \
+  FILE *fp; \
+  fp = fopen("/tmp/kmsg", "a"); \
+  if (fp) { \
+    fprintf(fp, format, ## args); \
+    fclose(fp); \
+    } \
 } while(0)
 #else
 #define DBG(format, args...)
@@ -60,87 +63,91 @@ int main(int argc, char **argv)
 int env_action_ezcd(int argc, char **argv)
 #endif
 {
-	char buf[256];
-	proc_stat_t *pidList;
-	int flag, ret;
+  char buf[256];
+  proc_stat_t *pidList;
+  int flag, ret;
+  char *name;
 
-	if (argc < 2) {
-		return (EXIT_FAILURE);
-	}
+  if (argc < 2) {
+    return (EXIT_FAILURE);
+  }
 
-	if (strcmp(argv[0], "ezcd")) {
-		return (EXIT_FAILURE);
-	}
+  name = strrchr(argv[0], '/');
+  name = name ? name+1 : argv[0];
+  if (strcmp(name, "ezcd")) {
+    return (EXIT_FAILURE);
+  }
 
-	if (utils_init_ezcfg_api(EZCD_CONFIG_FILE_PATH) == false) {
-		return (EXIT_FAILURE);
-	}
+  if (utils_init_ezcfg_api(EZCD_CONFIG_FILE_PATH) == false) {
+    DBG("%s(%d): init %s fail!\n", __func__, __LINE__, EZCD_CONFIG_FILE_PATH);
+    return (EXIT_FAILURE);
+  }
 
-	ret = EXIT_SUCCESS;
-	flag = utils_get_rc_act_type(argv[1]);
+  ret = EXIT_SUCCESS;
+  flag = utils_get_rc_act_type(argv[1]);
 
-	switch (flag) {
-	case RC_ACT_BOOT :
-		/* FIXME: nvram is not ready now!!! */
-		pop_etc_ezcd_conf(RC_ACT_BOOT);
-		/* ezcd config file should be prepared */
-		snprintf(buf, sizeof(buf), "%s -d", CMD_EZCD);
-		utils_system(buf);
+  switch (flag) {
+  case RC_ACT_BOOT :
+    /* FIXME: nvram is not ready now!!! */
+    pop_etc_ezcd_conf(RC_ACT_BOOT);
+    /* ezcd config file should be prepared */
+    snprintf(buf, sizeof(buf), "%s -d", CMD_EZCD);
+    utils_system(buf);
 
-		/* wait until nvram is ready */
-		if (utils_ezcd_wait_up(0) == false) {
-			ret = EXIT_FAILURE;
-		}
+    /* wait until nvram is ready */
+    if (utils_ezcd_wait_up(0) == false) {
+      ret = EXIT_FAILURE;
+    }
 
-		break;
+    break;
 
-	case RC_ACT_STOP :
-		pidList = utils_find_pid_by_name("ezcd");
-		while (pidList) {
-			int i;
-			for (i = 0; pidList[i].pid > 0; i++) {
-				kill(pidList[i].pid, SIGTERM);
-			}
-			free(pidList);
-			pidList = utils_find_pid_by_name("ezcd");
-		}
+  case RC_ACT_STOP :
+    pidList = utils_find_pid_by_name("ezcd");
+    while (pidList) {
+      int i;
+      for (i = 0; pidList[i].pid > 0; i++) {
+        kill(pidList[i].pid, SIGTERM);
+      }
+      free(pidList);
+      pidList = utils_find_pid_by_name("ezcd");
+    }
 
-		/* wait until ezcd is exit */
-		if (utils_ezcd_wait_down(0) == false) {
-			ret = EXIT_FAILURE;
-		}
+    /* wait until ezcd is exit */
+    if (utils_ezcd_wait_down(0) == false) {
+      ret = EXIT_FAILURE;
+    }
 
-		break;
+    break;
 
-	case RC_ACT_RELOAD :
-		/* re-generate ezcd config file */
-		pop_etc_ezcd_conf(flag);
-	#if 0
-		/* FIXME: don't commit, or it will overwrite the original data */
-		/* save the nvram changes before doing reload */
-		ezcfg_api_nvram_commit();
-	#endif
-		/* send signal to ezcd to reload config */
-		pidList = utils_find_pid_by_name("ezcd");
-		if (pidList) {
-			int i;
-			for (i = 0; pidList[i].pid > 0; i++) {
-				kill(pidList[i].pid, SIGUSR1);
-			}
-			free(pidList);
-		}
+  case RC_ACT_RELOAD :
+    /* re-generate ezcd config file */
+    pop_etc_ezcd_conf(flag);
+  #if 0
+    /* FIXME: don't commit, or it will overwrite the original data */
+    /* save the nvram changes before doing reload */
+    ezcfg_api_nvram_commit();
+  #endif
+    /* send signal to ezcd to reload config */
+    pidList = utils_find_pid_by_name("ezcd");
+    if (pidList) {
+      int i;
+      for (i = 0; pidList[i].pid > 0; i++) {
+        kill(pidList[i].pid, SIGUSR1);
+      }
+      free(pidList);
+    }
 
-		/* wait until nvram is ready */
-		if (utils_ezcd_wait_up(0) == false) {
-			ret = EXIT_FAILURE;
-		}
+    /* wait until nvram is ready */
+    if (utils_ezcd_wait_up(0) == false) {
+      ret = EXIT_FAILURE;
+    }
 
-		break;
+    break;
 
-	default:
-		ret = EXIT_FAILURE;
-		break;
-	}
+  default:
+    ret = EXIT_FAILURE;
+    break;
+  }
 
-	return (ret);
+  return (ret);
 }
